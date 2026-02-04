7671c8bb5701c08ca1c5b15f4037e5c2256ddf0c
Donvlepa <pajostef123@gmail.com>
2026-02-04 17:40:02 +0100

    Server uticnica

---
+++ Client/Client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class Client
+    {
+        static void Main(string[] args)
+        {
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Loopback, 9000);
+
+            try
+            {
+                clientSocket.Connect(serverEndPoint);
+                Console.WriteLine("Connected to server.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Connection failed: {ex.Message}");
+
+
+            }
+        }
+    }
+}
