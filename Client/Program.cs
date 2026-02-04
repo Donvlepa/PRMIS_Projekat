5825d3ffa0569b73a090e5c6265a4ac2f35e76a7
Donvlepa <pajostef123@gmail.com>
2026-02-04 17:27:28 +0100

    Serverska uticnica

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,10 @@ namespace Client
     {
         static void Main(string[] args)
         {
+            Socket serverSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Any, 9000);
+            serverSocket.Bind(serverEndPoint);
+            serverSocket.Listen(10);
         }
     }
 }
