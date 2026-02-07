677f85cf3f6eb3730df7831ab7af9264fee6c6d4
Migee12 <zilic1229@gmail.com>
2026-02-07 17:39:00 +0100

    dodata klasa igrac

--- Domain/Igrac.cs
+++ Domain/Igrac.cs
@@ -1,15 +1,50 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
 
 namespace Domain
 {
+    [Serializable]
     public class Igrac
     {
-        static void Main(string[] args)
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+        public int BrojPobeda { get; set; }
+        public int BrojOsvojenihBodova { get; set; }
+
+        public Igrac() { }
+
+        public Igrac(string ime, string prezime)
         {
+            Ime = ime;
+            Prezime = prezime;
+            BrojPobeda = 0;
+            BrojOsvojenihBodova = 0;
+        }
+
+        public byte[] Serijalizuj()
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            using (MemoryStream ms = new MemoryStream())
+            {
+                bf.Serialize(ms, this);
+                return ms.ToArray();
+            }
+        }
+
+        public static Igrac Deserijalizuj(byte[] data)
+        {
+            BinaryFormatter bf = new BinaryFormatter();
+            using (MemoryStream ms = new MemoryStream(data))
+            {
+                return (Igrac)bf.Deserialize(ms);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Ime} {Prezime} | Pobede: {BrojPobeda} | Bodovi: {BrojOsvojenihBodova}";
         }
     }
 }
